94633b65409b03e8739fc99c26a492eba6091b18
front-lucy <136558304+front-lucy@users.noreply.github.com>
2026-03-01 12:30:48 +0900

    Comment out skin care option in brand schema
    Commented out the skin care option in the list.

--- schemaTypes/brand.ts
+++ schemaTypes/brand.ts
@@ -31,7 +31,7 @@ export default defineType({
         list: [
           {title: 'Fragrance (フレグランス)', value: 'fragrance'},
           {title: 'Organic Cosmetics (オーガニックコスメ)', value: 'organic_cosmetics'},
-          // {title: 'Skin Care (スキンケア)', value: 'skin_care'},
+          // {title: 'Skin Care (スキンケア)', value: 'skin_care'}, 여기에 추가하시면 됩니다 : ) 
           // {title: 'Make Up (メイクアップ)', value: 'make_up'},
         ],
         layout: 'radio',
